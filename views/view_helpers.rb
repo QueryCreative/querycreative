6672ab61f5457641dccf3055e0bbe2b4549d0a48
Joshua Schmitz <js@the42.com>
2016-01-23 19:43:27 -0600

    updates, some style upgrades, add ''thin'' gem for better local server stuffs

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,10 +1,7 @@
-#
 # Methods placed in this module can be used inside of any view.
 # View helpers allow you to encapsalate complex logic and keep your
 # views pristine.
-#
 module ViewHelpers
-  
   # Calculate the years for a copyright
   def copyright_years(start_year)
     end_year = Date.today.year
@@ -14,12 +11,9 @@ module ViewHelpers
       "#{start_year}&#8211;#{end_year}"
     end
   end
-  
   # Handy for hiding a block of unfinished code
   def hidden(&block)
     #no-op
   end
-  
   # Add your own helpers below...
-  
 end
